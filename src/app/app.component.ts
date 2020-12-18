e53f4395aeac5704c6983c3f2ce879f559ecc6b9
rasheed-maker <rasheedabla09@gmail.com>
2020-12-18 17:27:46 +0100

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,57 @@
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit, AfterViewInit {
+  @ViewChild('canvas', { read: ElementRef, static: true })
+  canvas: ElementRef<HTMLCanvasElement>;
+  ctx: CanvasRenderingContext2D;
+
+  drawLine(n) {
+    this.ctx.moveTo(n, n);
+    this.ctx.lineTo(100, 100);
+    this.ctx.stroke();
+  }
+
+  drawCircle() {
+    this.ctx.beginPath();
+    this.ctx.arc(100, 100, 100, 0, 2 * Math.PI);
+    this.ctx.fill();
+  }
+
+  drawRect() {
+    this.ctx.fillStyle = 'blue';
+    this.ctx.fillRect(0, 0, 100, 100);
+  }
+
+  clearRect() {
+    this.ctx.clearRect(0, 0, 400, 400);
+  }
+
+  setWidth(width) {
+    this.canvas.nativeElement.width = +width;
+  }
+
+  setHeight(height) {
+    this.canvas.nativeElement.height = +height;
+  }
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    this.canvas.nativeElement.width = window.innerWidth;
+    this.canvas.nativeElement.height = window.innerHeight;
+    this.ctx = this.canvas.nativeElement.getContext('2d');
+  }
+}
